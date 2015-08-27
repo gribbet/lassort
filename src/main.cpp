11aa7b199cd9841014a4f93eab18945b28875d2a
gribbet <graham@airphrame.com>
2015-08-27 13:13:18 -0700

    Fix indentation

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ class Tile {
 public:
 	Tile(boost::filesystem::path workDir):
 		_count(0),
-        path(workDir / boost::filesystem::unique_path("%%%%-%%%%-%%%%-%%%%.las")),
+	path(workDir / boost::filesystem::unique_path("%%%%-%%%%-%%%%-%%%%.las")),
 		writer(NULL) {
 		ofs.open(path.string(), std::ios::out | std::ios::binary);
 	}
@@ -68,10 +68,10 @@ public:
 	long const count() {
 		return _count;
 	}
-    
-    uintmax_t const fileSize() {
-        return boost::filesystem::file_size(path);
-    }
+	
+	uintmax_t const fileSize() {
+		return boost::filesystem::file_size(path);
+	}
 
 	void write(liblas::Writer &writer) {
 		std::ifstream ifs(path.string(), std::ios::in | std::ios::binary);
@@ -102,11 +102,11 @@ private:
 
 class Grid {
 public:
-    Grid(std::string workDir, double tileSize):
-        tileSize(tileSize),
-        workDir(boost::filesystem::path(workDir)),
-        workDirCreated(boost::filesystem::create_directories(workDir)),
-        count(0) {
+	Grid(std::string workDir, double tileSize):
+		tileSize(tileSize),
+		workDir(boost::filesystem::path(workDir)),
+		workDirCreated(boost::filesystem::create_directories(workDir)),
+		count(0) {
 	}
 
 	~Grid() {
@@ -115,12 +115,12 @@ public:
 			it != tiles.end(); ++it)
 			delete it->second;
 		tiles.clear();
-        if (workDirCreated)
-            boost::filesystem::remove(workDir);
+		if (workDirCreated)
+			boost::filesystem::remove(workDir);
 	}
 
 	void read(liblas::Reader &reader) {
-        std::cout << "Tiled 0%" << std::flush;
+	std::cout << "Tiled 0%" << std::flush;
 		liblas::Header header = reader.GetHeader();
 		long total = header.GetPointRecordsCount();
 		long read = 0;
@@ -128,7 +128,7 @@ public:
 			add(reader.GetPoint());
 			if (++read % 1000000 == 0) {
 				flush();
-                std::cout << "\rTiled " << (100 * read / total) << "%" << std::flush;
+				std::cout << "\rTiled " << (100 * read / total) << "%" << std::flush;
 			}
 		}
 		flush();
@@ -137,7 +137,7 @@ public:
 	}
 
 	void write(liblas::Writer &writer) {
-        std::cout << "Merged 0%" << std::flush;
+		std::cout << "Merged 0%" << std::flush;
 		long written = 0;
 		long lastChunk = -1;
 		for(std::map<TileIndex, Tile*>::iterator it = tiles.begin();
@@ -152,31 +152,31 @@ public:
 
 		std::cout << "\rMerged 100%" << std::endl;
 	}
-    
-    long tileCount() {
-        return tiles.size();
-    }
-    
-    long averageTileCount() {
-        long total = 0;
-        for(std::map<TileIndex, Tile*>::iterator it = tiles.begin();
-            it != tiles.end(); ++it)
-            total += it->second->count();
-        return total /= tileCount();
-    }
-    
-    uintmax_t averageTileFileSize() {
-        uintmax_t total = 0;
-        for(std::map<TileIndex, Tile*>::iterator it = tiles.begin();
-            it != tiles.end(); ++it)
-            total += it->second->fileSize();
-        return total /= tileCount();
-    }
+	
+	long tileCount() {
+		return tiles.size();
+	}
+	
+	long averageTileCount() {
+		long total = 0;
+		for(std::map<TileIndex, Tile*>::iterator it = tiles.begin();
+			it != tiles.end(); ++it)
+			total += it->second->count();
+		return total /= tileCount();
+	}
+	
+	uintmax_t averageTileFileSize() {
+		uintmax_t total = 0;
+		for(std::map<TileIndex, Tile*>::iterator it = tiles.begin();
+			it != tiles.end(); ++it)
+			total += it->second->fileSize();
+		return total /= tileCount();
+	}
 
 private: 
 	const int tileSize;
-    const boost::filesystem::path workDir;
-    const bool workDirCreated;
+	const boost::filesystem::path workDir;
+	const bool workDirCreated;
 	long count;
 	std::map<TileIndex, Tile*> tiles;
 
@@ -196,14 +196,14 @@ private:
 
 class Sorter {
 public:
-    Sorter(std::string input,
-           std::string output,
-           std::string workDir,
-           double tileSize = 0.0):
+	Sorter(std::string input,
+		   std::string output,
+		   std::string workDir,
+		   double tileSize = 0.0):
 		input(input),
-        output(output),
-        workDir(workDir),
-        tileSize(tileSize) {
+		output(output),
+		workDir(workDir),
+		tileSize(tileSize) {
 	}
 
 	void sort() {
@@ -211,20 +211,20 @@ public:
 
 		liblas::ReaderFactory factory;
 		liblas::Reader reader = factory.CreateWithStream(ifs);
-        liblas::Header header = reader.GetHeader();
+		liblas::Header header = reader.GetHeader();
 		
-        double tileSize = this->tileSize;
-        if (tileSize == 0.0)
-            tileSize = estimateTileSize(reader);
-        
+		double tileSize = this->tileSize;
+		if (tileSize == 0.0)
+			tileSize = estimateTileSize(reader);
+		
 		Grid grid(workDir, tileSize);
 		grid.read(reader);
 
 		ifs.close();
-        
-        std::cout << "Total tiles: " << grid.tileCount() << std::endl;
-        std::cout << "Average tile count: " << grid.averageTileCount() << std::endl;
-        std::cout << "Average tile size: " << (grid.averageTileFileSize()/1000000) << "MB" << std::endl;
+		
+		std::cout << "Total tiles: " << grid.tileCount() << std::endl;
+		std::cout << "Average tile count: " << grid.averageTileCount() << std::endl;
+		std::cout << "Average tile size: " << (grid.averageTileFileSize()/1000000) << "MB" << std::endl;
 
 		std::ofstream ofs(output, std::ios::out | std::ios::binary);
 		liblas::Writer writer(ofs, header);
@@ -234,10 +234,10 @@ public:
 	}
 
 private:
-    const std::string input;
-    const std::string output;
-    const std::string workDir;
-    const double tileSize;
+	const std::string input;
+	const std::string output;
+	const std::string workDir;
+	const double tileSize;
 
 	double estimateTileSize(liblas::Reader reader) {
 		liblas::Header const& header = reader.GetHeader();
@@ -255,38 +255,38 @@ private:
 };
 
 int main(int argc, char **argv) {
-    double tileSize;
-    std::string input;
-    std::string output;
-    std::string workDir;
+	double tileSize;
+	std::string input;
+	std::string output;
+	std::string workDir;
 
 	po::options_description desc("options");
-    po::positional_options_description p;
-    po::variables_map vm;
+	po::positional_options_description p;
+	po::variables_map vm;
 	desc.add_options() 
 		("help,h", "Prints usage")
-        ("size,s", po::value<double>(&tileSize)->default_value(0.0), "Tile size")
-        ("input,i", po::value<std::string>(&input), "Input LAS file")
-        ("output,o", po::value<std::string>(&output)->default_value("sorted.las"), "Output LAS file")
-        ("work-dir,w", po::value<std::string>(&workDir)->default_value("temp"));
-    p.add("input", 1);
-    p.add("output", 1);
-    
-    try {
-        po::store(po::command_line_parser(argc, argv)
-            .options(desc)
-            .positional(p)
-            .run(), vm);
-        po::notify(vm);
+		("size,s", po::value<double>(&tileSize)->default_value(0.0), "Tile size")
+		("input,i", po::value<std::string>(&input), "Input LAS file")
+		("output,o", po::value<std::string>(&output)->default_value("sorted.las"), "Output LAS file")
+		("work-dir,w", po::value<std::string>(&workDir)->default_value("temp"));
+	p.add("input", 1);
+	p.add("output", 1);
+	
+	try {
+		po::store(po::command_line_parser(argc, argv)
+			.options(desc)
+			.positional(p)
+			.run(), vm);
+		po::notify(vm);
 
-        if(vm.count("help") || !vm.count("input")) {
-            std::cout<< "USAGE: lassort [options] input" << std::endl;
-            std::cout << desc << std::endl;
-            return 0;
-        }
-        
-        Sorter(input, output, workDir, tileSize).sort();
-        return 0;
+		if(vm.count("help") || !vm.count("input")) {
+			std::cout<< "USAGE: lassort [options] input" << std::endl;
+			std::cout << desc << std::endl;
+			return 0;
+		}
+		
+		Sorter(input, output, workDir, tileSize).sort();
+		return 0;
 				
 	} catch (std::exception &e) {
 		std::cerr << "Error: " << e.what() << std::endl;
